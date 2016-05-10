f313e875148f900bb9cbc334e86690633515f4b1
vpsingh1984 <psingh.vijay@yahoo.com>
2016-05-10 16:24:17 +0530

    ui-router added

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,32 +1,52 @@
 var ekartApp = angular.module('Ekart', 
 	[
 		'ngRoute',
-		'appControllers',
-		'availabilityFilter',
-		'itemServices',
-		'angularFileUpload'
+		'ui.router'
 	]
 );
 
-ekartApp.config(['$routeProvider', function($routeProvider){
-	$routeProvider.
-		when('/', {
-			templateUrl: 'partials/item-list.html',
-			controller: 'HomeController'
-		}).
-		when('/:id', {
-			templateUrl: 'partials/item-details.html',
-			controller: 'ItemController'
-		}).
-		when('/items/:id/edit', {
-			templateUrl: 'partials/item-form.html',
-			controller: 'EditItemController'
-		}).
-		when('/items/new', {
-			templateUrl: 'partials/item-form.html',
-			controller: 'NewItemController'
-		}).
-		otherwise({
-			redirectTo: '/'
-		})
+// ekartApp.config(['$routeProvider', function($routeProvider){
+// 	$routeProvider.
+// 		when('/', {
+// 			templateUrl: 'partials/item-list.html',
+// 			controller: 'HomeController'
+// 		}).
+// 		when('/:id', {
+// 			templateUrl: 'partials/item-details.html',
+// 			controller: 'ItemController'
+// 		}).
+// 		when('/items/:id/edit', {
+// 			templateUrl: 'partials/item-form.html',
+// 			controller: 'EditItemController'
+// 		}).
+// 		when('/items/new', {
+// 			templateUrl: 'partials/item-form.html',
+// 			controller: 'NewItemController'
+// 		}).
+// 		otherwise({
+// 			redirectTo: '/'
+// 		})
+// }]);
+
+ekartApp.config(['$stateProvider','$urlRouterProvider',function($stateProvider, $urlRouterProvider) {
+    
+    $urlRouterProvider.otherwise('app/views/home/index.html');
+    
+    $stateProvider
+        
+        // HOME STATES AND NESTED VIEWS ========================================
+        .state('home', {
+            url: '/',
+            templateUrl: 'app/views/home/index.html'
+        })
+        .state('home.allquestion', {
+            url: '/allquestion',
+            templateUrl: 'app/views/all-question/all-question.html'
+        })
+        
+        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+        .state('about', {
+            // we'll get to this in a bit       
+        });
+        
 }]);
